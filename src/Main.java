b9a51426c4dd84b60ddc267acdfd6cc4bc9d9ea6
Zhoomart Erkinbek uulu <zhoma.erkinbek@gmail.com>
2022-09-10 18:39:44 +0600

    delete unnecessary imports

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,7 @@
 import controlwork.CalendarModel;
 import controlwork.Control;
-import controlwork.Task;
-import controlwork.TypeOfTask;
 
 import java.io.IOException;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.format.TextStyle;
-import java.util.Arrays;
-import java.util.Locale;
 
 public class Main {
     private static CalendarModel calendarModel = new CalendarModel();
